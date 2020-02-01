10a7486814bae5eec07e6092385ca5efb3e712b9
James Lovejoy <james.lovejoy2@gmail.com>
2020-02-01 12:58:48 -0600

    added vomit column and category for food, attempting to add sound effects to clicks

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,14 +1,31 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 //Document.ready function but for jquery
 $(function() {
+
+  const vomit_sound = $("#vomit_sound")[0];
+  const eatSound = $("#eat_sound")[0];
+
+  // console.log(eatSound);
+  // console.log(audio);
+ 
     $(".change-eaten").on("click", function(event) {
+      console.log(">>>>>>>  begin change-eaten")
       const id = $(this).data("id");
       const newEaten = $(this).data("neweaten");
         console.log($(this));
       const newEatenState = {
         eaten: newEaten,
       };
-  
+
+      console.log(newEaten);
+      if(newEaten) {
+        console.log("eat");
+      eatSound.play();
+      } else {
+        console.log("throw up");
+        vomit_sound.play()
+      }
+
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
@@ -17,7 +34,7 @@ $(function() {
         function() {
           console.log("changed eaten to", newEaten);
           // Reload the page to get the updated list
-          location.reload();
+          timeoutReload();
         }
       );
     });
@@ -28,10 +45,10 @@ $(function() {
 
         
       if($("#burger_name").val().trim() === '') {
-        $('.modal').modal('show');
+        $('.order').modal('show');
 
         //this line of code grabs the modal div and opens it
-        $("#myModal").modal('open');
+        $("#orderSomething").modal('open');
       }
 
       console.log($("#burger_name"));
@@ -41,9 +58,6 @@ $(function() {
         vomit_amt: 0,
         // $("[name=eaten]:checked").val().trim()
       }
-    
-
-    //   console.log(newBurger);
 
       // Send the POST request.
       $.ajax("/api/burgers", {
@@ -53,14 +67,14 @@ $(function() {
         function() {
           console.log("created new burger");
           // Reload the page to get the updated list
-          location.reload();
+          timeoutReload();
         }
       );
     });
   
     $(".delete-burger").on("click", function(event) {
       var id = $(this).data("id");
-  
+
       // Send the DELETE request.
       $.ajax("/api/burgers/" + id, {
         type: "DELETE"
@@ -68,9 +82,28 @@ $(function() {
         function() {
           console.log("deleted burger", id);
           // Reload the page to get the updated list
-          location.reload();
+          openGrossModal();     
         }
       );
     });
+
+    //This is an on-clik on the modal that shows up when you try to eat vomitted burger
+    //It will reload the page so the deleted burger is no longer shown.
+    //It was already deleted in earlier step of clicking on eat it button for vomitted burger
+    $(".delete-gross").on("click", function (event){
+      location.reload();
+    })
+
   });
   
+
+  function openGrossModal() {
+    $('.gross-delete').modal('show');
+
+    //this line of code grabs the modal div and opens it
+    $("#gross").modal('open');
+  }
+
+  function timeoutReload() {
+    setTimeout(location.reload(), 3000);
+  }
